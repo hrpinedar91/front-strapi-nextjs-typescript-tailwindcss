f9e31265384a3f907faf4c6f80a3ca188bd1658c
Harold Pineda <hrpinedar91@gmail.com>
2023-11-05 20:00:34 -0500

    firt commit preconfig

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { fetchApi } from "@/helpers/fetch-api";
+
+const getData = async () => {
+  const path = "/blog";
+  const urlParams = {
+    populate: "*",
+    sort: {
+      createAt: "asc",
+    },
+    pagination: {
+      page: 1,
+      pageSize: 2,
+    },
+  };
+
+  const data = fetchApi(path, urlParams);
+  return data;
+};
+
+const Blog = async () => {
+  const data = await getData();
+  return (
+    <div>
+      <h1>Pagina del blog</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default Blog;
