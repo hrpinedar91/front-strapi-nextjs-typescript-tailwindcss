f3a0404760e27af60e5879f8e1fc74f7e0971edd
Harold Pineda <hrpinedar91@gmail.com>
2023-11-07 22:29:18 -0500

    delete rules eslint

--- src/components/PagePagination.tsx
+++ src/components/PagePagination.tsx
@@ -23,7 +23,7 @@ const Pagination = ({ pagination }: Props) => {
     "px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white";
 
   return (
-    <nav aria-label="Page navigation example">
+    <nav aria-label="Page navigation example" className="text-center">
       <ul className="inline-flex -space-x-px">
         <li>
           <Link
